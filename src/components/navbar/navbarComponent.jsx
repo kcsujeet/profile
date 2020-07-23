e81902dd567f58b70e329d900ec152f27dad71dd
Sujeet Kc <sujeetkc45@gmail.com>
2020-07-23 11:03:25 +0545

    mobile ui change

--- src/components/navbar/navbarComponent.jsx
+++ src/components/navbar/navbarComponent.jsx
@@ -1,22 +1,30 @@
 import React, {useState, useEffect} from 'react'
-import {Menu, Switch} from 'antd'
+import {Button, Switch} from 'antd'
 
 import './navbarComponent.scss'
 
 function Navbar(){
-    const [headerTheme, setHeaderTheme] = useState('dark')
-    const [selectedKey, setSelectedKey] = useState('home')
+    const [activeLink, setActiveLink] = useState('about')
     const [blockPosition, setBlockPosition] = useState('center')
     const [homeElement, setHomeElement] = useState('')
     const [skillsElement, setSkillsElement] = useState('')
     const [experienceElement, setExperienceElement] = useState('')
     const [contactElement, setContactElement] = useState('')
 
-    const handleClick = (e)=>{
-        const anchor = document.getElementById(`${e.key}`)
-        anchor.scrollIntoView({ behavior: 'smooth', block: blockPosition})
-        setSelectedKey(`${e.key}`)
+    const handleClick = (event)=>{
+        if(window.innerWidth <= 760){
+            handleHamburgerClick()
+            setTimeout((target) => {
+                const anchor = document.getElementById(`${target.name}`)
+                anchor.scrollIntoView({ behavior: 'smooth', block: blockPosition})
+            }, 1000, event.target);
+        }else{
+            const anchor = document.getElementById(`${event.target.name}`)
+            anchor.scrollIntoView({ behavior: 'smooth', block: blockPosition})
+        }
+        
     }
+    
 
     useEffect(()=>{
         var saved_theme = localStorage.getItem('kcsujeet_theme')
@@ -45,13 +53,13 @@ function Navbar(){
 
     const handleSroll = ()=>{
         if(isScrolledIntoView(homeElement)){
-            setSelectedKey('home')
+            setActiveLink('about')
         }else if(isScrolledIntoView(skillsElement)){
-            setSelectedKey('skills')
+            setActiveLink('skills')
         }else if(isScrolledIntoView(experienceElement)){
-            setSelectedKey('experience')
+            setActiveLink('experience')
         }else if(isScrolledIntoView(contactElement)){
-            setSelectedKey('contact')
+            setActiveLink('contact')
         }
     }
 
@@ -66,48 +74,68 @@ function Navbar(){
       )
     }
 
-    const switchTheme= (checked, event)=>{
+    const switchTheme= (checked)=>{
         if(!checked){
-            setHeaderTheme('light')
-            document.querySelector('g').setAttribute('fill','rgba(0, 0, 0, 0.65)') // fill graphql logo
-            var elems = [...document.getElementsByClassName('darkTheme')]
-            elems.forEach(element => {
-                element.classList.remove('darkTheme')
-                element.classList.add('lightTheme')
-                localStorage.setItem('kcsujeet_theme', 'lightTheme')
-            });
+            document.getElementsByTagName('body')[0].classList.replace('darkTheme', 'lightTheme')
+            localStorage.setItem('kcsujeet_theme', 'lightTheme')
             
         }else{
-            setHeaderTheme('dark')
-            document.querySelector('g').setAttribute('fill','#fff') // fill graphql logo
-            var elements = [...document.getElementsByClassName('lightTheme')]
-            elements.forEach(element => {
-                element.classList.remove('lightTheme')
-                element.classList.add('darkTheme')
-                localStorage.setItem('kcsujeet_theme', 'darkTheme')
-            });
+            document.getElementsByTagName('body')[0].classList.replace('lightTheme', 'darkTheme')
+            localStorage.setItem('kcsujeet_theme', 'darkTheme')
         }
     }
 
+    const handleSwitchTheme =(checked)=>{
+        document.getElementById('theme-dropdown-toggle').blur()
+        if(window.innerWidth <= 760){
+            handleHamburgerClick() 
+        } 
+       switchTheme(checked)
+    }
+
+    const handleHamburgerClick = ()=>{
+        var nav = document.querySelector('.navbar-nav')
+        var hamburger = document.querySelector('.hamburger')
+        nav.classList.toggle('open')
+        hamburger.classList.toggle('open')
+    }
+
     return(
-        <Menu theme={headerTheme} mode="horizontal" selectedKeys={[selectedKey]} className='navbar'>
-            <Menu.Item onClick={handleClick} key="home">
-                About
-            </Menu.Item>
-            <Menu.Item onClick={handleClick} key="skills">
-                Skills
-            </Menu.Item>
-            <Menu.Item onClick={handleClick} key="experience">
-                Experience
-            </Menu.Item>
-            <Menu.Item onClick={handleClick} key="contact">
-                Contact
-            </Menu.Item>
-            <Menu.Item>
-                Theme 
-                <Switch onChange={switchTheme} defaultChecked={localStorage.getItem('kcsujeet_theme') === 'darkTheme'} style={{"marginLeft": ".5rem"}}/>
-            </Menu.Item>
-      </Menu>
+        <nav className="navbar">
+            <div className="hamburger" onClick={handleHamburgerClick}>
+                <div className="hamburger-line"></div>
+            </div>
+
+            <ul className="navbar-nav">
+                <li className={`nav-item`}>
+                    <a name="home" onClick={handleClick} className={`nav-link ${activeLink === 'about' ? 'active' : ''}`}>About</a>
+                </li>
+                <li className={`nav-item`}>
+                    <a name="skills" onClick={handleClick} className={`nav-link  ${activeLink === 'skills' ? 'active' : ''}`}>Skills</a>
+                </li>
+                <li className={`nav-item `}>
+                    <a name="experience" onClick={handleClick} className={`nav-link ${activeLink === 'experience' ? 'active' : ''}`}>Experience</a>
+                </li>
+                <li className={`nav-item `}>
+                    <a name="contact" onClick={handleClick} className={`nav-link ${activeLink === 'contact' ? 'active' : ''}`}>Contact</a>
+                </li>
+                <li name="theme" className={"nav-item has-dropdown"}>
+                    <Button id="theme-dropdown-toggle" type="text" className="nav-link">Theme</Button>
+                    <ul className="theme-dropdown">
+                        <li className="theme-option">
+                            <Button type="text" onClick={()=>handleSwitchTheme(false)} id="lightTheme">
+                                <i className="fa fa-circle light-circle" ></i>
+                                Light</Button>
+                        </li>
+                        <li className="theme-option">
+                            <Button type="text"  onClick={()=>handleSwitchTheme(true)} id="DarkTheme">
+                            <i className="fa fa-circle dark-circle" ></i>
+                                Dark</Button>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
     )
 }
 
