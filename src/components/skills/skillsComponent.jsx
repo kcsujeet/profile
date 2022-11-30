0e14df7681f63ee71b9b03f1cd3201a0d662a068
Sujeet Kc <sujeetkc45@gmail.com>
2022-11-30 16:05:33 -0400

    skills updated

--- src/components/skills/skillsComponent.jsx
+++ src/components/skills/skillsComponent.jsx
@@ -33,44 +33,47 @@ function Skills({id}){
                         I'm proficient in the following technologies.
                     </p>
                     <Row gutter={16}>
-                       
-                        <Col className='icon-col' md={6} lg={6} sm={12} xs={12}>
-                            <Card className='icon-card text-align-center' hoverable cover={<GraphqlSvg style={{height: '43px'}}/>} style={{paddingTop: '.5rem'}}>
-                                <Meta description={<p >GraphQL</p>}></Meta>
-                            </Card>
-                        </Col>
-                        <Col className='icon-col' md={6} lg={6} sm={12} xs={12}>
-                            <Card className='icon-card  text-align-center' hoverable cover={<i className='fab fa-3x fa-react icon-margin-top'></i>}>
-                                <Meta description={<p >React</p>}></Meta>
-                            </Card>
-                        </Col>
-                                    
-                        <Col className='icon-col' md={6} lg={6} sm={12} xs={12}>
-                            <Card className='icon-card  text-align-center' hoverable cover={<i className='fab fa-3x fa-angular icon-margin-top'></i>}>
-                                <Meta description={<p >Angular</p>}></Meta>
-                            </Card>
-                        </Col>
-                        <Col className='icon-col' md={6} lg={6} sm={12} xs={12}>
-                            <Card className='icon-card  text-align-center' hoverable cover={<i className='fab fa-3x fa-vuejs icon-margin-top'></i>}>
-                                <Meta description={<p >Vue</p>}></Meta>
-                            </Card>
-                        </Col>
-                        <Col className='icon-col' md={6} lg={6} sm={12} xs={12}>
-                            <Card className='icon-card  text-align-center' hoverable cover={<i className='fas fa-3x fa-gem icon-margin-top'></i>}>
-                                <Meta description={<p >ROR</p>}></Meta>
-                            </Card>
-                        </Col>
-                        <Col className='icon-col' md={6} lg={6} sm={12} xs={12}>
-                            <Card className='icon-card  text-align-center' hoverable cover={<i className='fab fa-3x fa-node-js icon-margin-top'></i>}>
-                                <Meta description={<p >Nodejs</p>}></Meta>
-                            </Card>
-                        </Col>
-                        <Col className='icon-col' md={6} lg={6} sm={12} xs={12}>
+                        <Col className='icon-col' md={8} lg={8} sm={12} xs={12}>
                             <Card className='icon-card  text-align-center' hoverable cover={<i className='fab fa-3x fa-js icon-margin-top'></i>}>
                                 <Meta description={<p >Javascript</p>}></Meta>
                             </Card>
                         </Col>
-                        <Col className='icon-col' md={6} lg={6} sm={12} xs={12}>
+                        <Col className='icon-col' md={8} lg={8} sm={12} xs={12}>
+                            <Card className='icon-card text-align-center' hoverable cover={<i className='fab fa-3x fa-html5 icon-margin-top'></i>}>
+                                <Meta description={<p >HTML</p>}></Meta>
+                            </Card>
+                        </Col>
+                        <Col className='icon-col' md={8} lg={8} sm={12} xs={12}>
+                            <Card className='icon-card text-align-center' hoverable cover={<i className='fab fa-3x fa-css3-alt icon-margin-top'></i>}>
+                                <Meta description={<p >CSS</p>}></Meta>
+                            </Card>
+                        </Col>
+                        <Col className='icon-col' md={8} lg={8} sm={12} xs={12}>
+                            <Card className='icon-card  text-align-center' hoverable cover={<i className='fab fa-3x fa-angular icon-margin-top'></i>}>
+                                <Meta description={<p >Angular</p>}></Meta>
+                            </Card>
+                        </Col>
+                        <Col className='icon-col' md={8} lg={8} sm={12} xs={12}>
+                            <Card className='icon-card  text-align-center' hoverable cover={<i className='fab fa-3x fa-vuejs icon-margin-top'></i>}>
+                                <Meta description={<p >Vue</p>}></Meta>
+                            </Card>
+                        </Col>
+                        <Col className='icon-col' md={8} lg={8} sm={12} xs={12}>
+                            <Card className='icon-card  text-align-center' hoverable cover={<i className='fab fa-3x fa-react icon-margin-top'></i>}>
+                                <Meta description={<p >React</p>}></Meta>
+                            </Card>
+                        </Col>
+                        <Col className='icon-col' md={8} lg={8} sm={12} xs={12}>
+                            <Card className='icon-card  text-align-center' hoverable cover={<i className='fas fa-3x fa-gem icon-margin-top'></i>}>
+                                <Meta description={<p >ROR</p>}></Meta>
+                            </Card>
+                        </Col>
+                        <Col className='icon-col' md={8} lg={8} sm={12} xs={12}>
+                            <Card className='icon-card  text-align-center' hoverable cover={<i className='fab fa-3x fa-node-js icon-margin-top'></i>}>
+                                <Meta description={<p >Node</p>}></Meta>
+                            </Card>
+                        </Col>
+                        <Col className='icon-col' md={8} lg={8} sm={12} xs={12}>
                             <Card className='icon-card  text-align-center' hoverable cover={<i className='fab fa-3x fa-python icon-margin-top'></i>}>
                                 <Meta description={<p >Python</p>}></Meta>
                             </Card>
@@ -78,9 +81,9 @@ function Skills({id}){
                     </Row>
                 </Space>
                 </Fade>
-            </Col>            
+            </Col>
         </Row>
-    )   
+    )
 }
 
 export default Skills
